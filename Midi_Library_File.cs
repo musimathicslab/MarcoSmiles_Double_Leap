9ba7c4b147ef35a7280ba7d0a0f07a99344acfce
DanieleSalerno <72736111+DanieleSalerno@users.noreply.github.com>
2023-06-06 00:00:42 +0200

    Adding MidiLib.dll and Midi_Library_File.cs Co-authored-by: Benedetto Simone <sim.benede@gmail.com>

---
+++ Midi_Library_File.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Sanford.Multimedia.Midi;
+
+
+
+namespace MidiLib
+{
+    public class MidiClass
+    {
+        private OutputDevice? outD;
+        private ChannelMessageBuilder? builder;
+
+
+        public MidiClass()
+        {
+            builder = new ChannelMessageBuilder();
+
+        }
+
+
+        //SendEvent allow to send a midi event (note_on/note_off) to the MarcoSmiles port 
+        public string SendEvent(int note, int octave, OutputDevice outDev, string command)
+        {
+            //To print the note 
+            String note_str = "";
+            String octave_str = "";
+            String[] note_names = new String[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+
+            //Data1 => MIDI note index
+            builder.Data1 = note + (octave * 12);
+
+            //Data2 => Velocity
+            builder.Data2 = 105;
+
+
+            builder.MidiChannel = 0;
+
+            //chosing the command
+            if (command.Equals("on"))
+            {
+                builder.Command = ChannelCommand.NoteOn;
+            }
+            else if (command.Equals("off"))
+            {
+                builder.Command = ChannelCommand.NoteOff;
+            }
+
+            //Building the message
+            builder.Build();
+            //Sending the message 
+            outDev.Send(builder.Result);
+
+            //Printing the note on terminal
+            note_str = note_names[note % 12];
+            if (note > 11)
+            {
+                octave_str = "" + (octave + 1);
+            }
+            else
+            {
+                octave_str = "" + octave;
+            }
+            string note_sent = command + note_str + octave_str;
+            return note_sent;
+
+        }
+
+        //OutPutDevice allow to find the MIDI port of MarcoSmiles
+        public OutputDevice FindMidi()
+        {
+            int DevId = 0;
+
+            //find MarcoSmiles port MIDI
+            int numDevice = OutputDevice.DeviceCount;
+
+
+            for (int i = 0; i < numDevice; i++)
+            {
+                MidiOutCaps dev = OutputDevice.GetDeviceCapabilities(i);
+
+                if (dev.name == "MarcoSmiles")
+                {
+                    DevId = i;
+
+
+                }
+            }
+
+
+            //Select the output Device 
+            outD = new OutputDevice(DevId);
+            return outD;
+
+        }
+    }
+}
---
+++ Raspberry/MidiClass.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using Sanford.Multimedia.Midi;
+using System;
+
+namespace MidiUtils
+{
+    public class MidiClass
+    {
+
+
+        /*
+         * Per eseguire il programma si deve scaricare ed installare LoopMidi
+         * --------------------------------------------------------------------
+         * https://www.tobias-erichsen.de/software/loopmidi.html
+         * --------------------------------------------------------------------
+         * Successivamente creare con LoopMidi una porta Midi virtuale con il nome "MarcoSmiles"
+         */
+        private OutputDevice outD;
+        private ChannelMessageBuilder builder;
+
+        public MidiClass()
+        {
+
+        }
+
+
+        public void SendEvent(int note, int octave)
+        {
+            //Per stampare a video la nota suonata
+            String Nota = "";
+            String Ottava = "";
+            String[] NomeNote = new String[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+
+
+            // C C# D D# E F F# G G# A A# B
+            // 0 1  2 3  4 5 6  7 8  9 10 11 ... 23
+            //aggiungo 12 per ogni ottava
+            //MarcoSmiles utilizza una tastiera con 2 ottave numerando le note selezionate da 0 a 23
+            //inoltre offre la possibilità di selezionare le ottave partendo dal Range C0-C1
+            //C0-C1  C1-C2  C2-C3 ....
+            //  0      1      2
+
+
+
+
+
+            //Per disabilitare tutti gli eventi MIDI in caso di errore
+            /*for (int i = 0; i < 128; i++)
+            {
+                //-----costruisco l'evento midi da inviare ---
+
+                //Data1 rappresenta la Nota
+
+                builder.Data1 = i;
+
+
+                //Data2 è la velocity
+                builder.Data2 = 120;
+
+                builder.MidiChannel = 0;
+
+                //Suono la nota
+                builder.Command = ChannelCommand.NoteOff;
+                builder.Build();
+                outD.Send(builder.Result);
+            }
+            */
+
+
+            //TO DO
+            // per integrare questo modulo in MarcoSmiles faccio corrispondere il Note On nel momento in cui viene selezionata la nota
+            // invio il note Off nel momento in cui si seleziona un'altra nota
+
+
+            //-----costruisco l'evento midi da inviare ---
+
+            Debug.Log("Note ON" + (note + (octave * 12)));
+            //Data1 rappresenta la Nota
+            builder.Data1 = note + (octave * 12);
+
+            //Data2 è la velocity
+            builder.Data2 = 105;
+
+            builder.MidiChannel = 0;
+
+            //Suono la nota
+            builder.Command = ChannelCommand.NoteOn;
+            builder.Build();
+            outD.Send(builder.Result);
+
+            //Stampo sul terminale la nota
+            Nota = NomeNote[note % 12];
+            if (note > 11)
+            {
+                Ottava = "" + (octave + 1);
+            }
+            else
+            {
+                Ottava = "" + octave;
+            }
+            Debug.Log("\n" + Nota + Ottava);
+
+        }
+
+
+        public void FindMidi()
+        {
+            int DevId = 0;
+
+            //Identifico la porta relativa a MarcoSmiles
+            int numDevice = OutputDevice.DeviceCount;
+            Debug.Log("\n Output Devices Disponibili: " + numDevice);
+
+            for (int i = 0; i < numDevice; i++)
+            {
+                MidiOutCaps dev = OutputDevice.GetDeviceCapabilities(i);
+                Debug.Log("\n" + dev.name);
+                if (dev.name == "MarcoSmiles")
+                {
+                    DevId = i;
+                    Debug.Log("\nPort Marco " + DevId);
+
+                }
+            }
+
+
+            //seleziono L'output Device e inizializzo Il MessageBuilder
+            outD = new OutputDevice(DevId);
+            builder = new ChannelMessageBuilder();
+
+        }
+
+
+
+        public void SendMidiOff(int note, int octave)
+        {
+
+
+            Debug.Log("Note OFF" + (note + (octave * 12)));
+            //Data1 rappresenta la Nota
+            builder.Data1 = (note + (octave * 12));
+
+            //Data2 è la velocity
+            builder.Data2 = 105;
+            builder.MidiChannel = 0;
+            //Invio il segnale di Note Off
+            builder.Command = ChannelCommand.NoteOff;
+            builder.Build();
+
+            outD.Send(builder.Result);
+
+
+
+
+
+
+
+        }
+
+    }
+}

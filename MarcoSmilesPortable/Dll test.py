4843e5b1826ed6d28d831bb83b941827cc8a4e00
DanieleSalerno <72736111+DanieleSalerno@users.noreply.github.com>
2023-06-05 23:23:29 +0200

    Merge of 2 method of MidiLib and cleaning code + comment Co-authored-by: Benedetto Simone <sim.benede@gmail.com>

--- MarcoSmilesPortable/Dll test.py
+++ MarcoSmilesPortable/Dll test.py
@@ -1,16 +1,16 @@
 import clr
-clr.AddReference("dlls/MidiUtils")
+clr.AddReference('dlls/MidiLib')
 import time
-from MidiUtils import MidiClass
+from MidiLib import MidiClass
 
 Midi = MidiClass()
 outdevice = Midi.FindMidi()
 for i in range (0,10):
 
-    note_sent=Midi.SendEvent(i,1,outdevice)
+    note_sent=Midi.SendEvent(i,1,outdevice,"on")
     print(note_sent)
 
     time.sleep(1)
 
-    note = Midi.SendMidiOff(i,1,outdevice)
+    note = Midi.SendEvent(i,1,outdevice,"off")
     print(note)
